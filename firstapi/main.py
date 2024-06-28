3f264bb75c473f02ed6ee7b05643729190ca6a71
Gabriel Reis Ruela <gabrielreis@vixdynamics.com.br>
2024-06-28 16:09:18 -0300

    Add any methods

--- firstapi/main.py
+++ firstapi/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException,status
+from fastapi.responses import JSONResponse
+from models import Curso
 
 app = FastAPI()
 
@@ -27,6 +29,33 @@ async def getCursoPorId(id: int):
     except KeyError:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="Não encontrado")
+        
+@app.post('/cursos', status_code=status.HTTP_201_CREATED)
+async def postCurso(curso: Curso):
+    if curso.id not in cursos:
+        cursos[curso.id] = curso
+        return curso
+    else:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                            detail="Curso já existente")
+
+@app.put('/cursos/{id}', status_code=status.HTTP_202_ACCEPTED)
+async def putCurso(id: int, curso: Curso):
+   if id in cursos:
+        cursos[id] = curso
+        return curso
+   else:
+       raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                           detail="Curso não existente com este id")
+
+@app.delete('/cursos/{id}')
+async def deleteCurso(id: int):
+    if id in cursos:
+        del cursos[id]
+        return JSONResponse(status_code=status.HTTP_200_OK, content=None)
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Curso não existente com este id")
 
 if __name__ == '__main__':
     import uvicorn
@@ -35,6 +64,5 @@ if __name__ == '__main__':
                 host="0.0.0.0",
                 port=8000,
                 log_level="info",
-                reload=True,
-                debug=True
+                reload=True
                 )
